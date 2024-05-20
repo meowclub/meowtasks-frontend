9a8629a32b8ea79702cb93050c3b8e22ffebd433
alemndev <140274341+alemndev@users.noreply.github.com>
2024-05-20 18:46:25 -0500

    refactor(pages): improved structure of the register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest.service';
 
+interface IUtilResponse {
+  name: string;
+  value: string
+}
+
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -9,8 +14,8 @@ import { RestService } from '../../services/rest.service';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
-  public avatars:string[] = [];
-  public colors:string[] = [];
+  public avatars:IUtilResponse[] = [];
+  public colors:IUtilResponse[] = [];
   public checkedAvatar:number = 0;
   public checkedColor:number = 0;
   
@@ -20,8 +25,13 @@ export class RegisterComponent implements OnInit {
     const avatars = JSON.parse((await this.rest.getAvatars()).data);
     const colors = JSON.parse((await this.rest.getColors()).data);
     
-    this.avatars = Object.keys(avatars).map(key => `url(${avatars[key]}) center`)
-    this.colors = Object.keys(colors).map(key => colors[key]);
+    this.avatars = Object.keys(avatars).map(key => { 
+      return { name: key, value: `url(${avatars[key]}) center` } 
+    })
+
+    this.colors = Object.keys(colors).map(key => { 
+      return { name: key, value: colors[key] } 
+    });
 
     // Random checked input
     this.checkedAvatar = Math.floor(Math.random() * (this.avatars.length - 0));
