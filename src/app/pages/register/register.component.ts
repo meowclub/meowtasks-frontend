77bb91530877c8d3ec5d52647b141eca00e195a8
alemndev <140274341+alemndev@users.noreply.github.com>
2024-05-21 17:42:23 -0500

    feat(pages): implemented meowUser register
    - Added register method in rest service.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ListenerService } from '../../services/listener.service';
+import IAlert from '../../interfaces/IAlert';
 
 interface IUtilResponse {
   name: string;
@@ -9,7 +12,7 @@ interface IUtilResponse {
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -18,10 +21,31 @@ export class RegisterComponent implements OnInit {
   public colors:IUtilResponse[] = [];
   public checkedAvatar:number = 0;
   public checkedColor:number = 0;
+  public form:FormGroup = new FormGroup({
+    nickname: new FormControl('', [
+      Validators.required,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+    ]),
+    passwordHint: new FormControl('', [
+      Validators.required,
+    ]),
+    color: new FormControl('', [
+      Validators.required,
+    ]),
+    avatar: new FormControl('', [
+      Validators.required,
+    ])
+  })
   
-  constructor(private rest: RestService) {}
+  constructor(private rest: RestService, private listener: ListenerService) {}
 
   async ngOnInit(): Promise<void> {
+    // Avatars and colors selection
     const avatars = JSON.parse((await this.rest.getAvatars()).data);
     const colors = JSON.parse((await this.rest.getColors()).data);
     
@@ -36,6 +60,27 @@ export class RegisterComponent implements OnInit {
     // Random checked input
     this.checkedAvatar = Math.floor(Math.random() * (this.avatars.length - 0));
     this.checkedColor = Math.floor(Math.random() * (this.colors.length - 0));
+  }
+
+  onRegister() {
+    this.rest.register(this.form.value).subscribe(rsp => {
+      const alert: IAlert = {
+        alerTitle: 'Welcome to meowTasks',
+        alertContent: rsp.msg,
+        alertType: 'success'
+      }
+
+      this.listener.newAlert.emit(alert)
+    }, 
+    err => {
+      const alert: IAlert = {
+        alerTitle: 'Error ocurred',
+        alertContent: err.error.msg,
+        alertType: 'danger'
+      }
+
+      this.listener.newAlert.emit(alert)
+    })
   }
 }
  
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 const API_ENDPOINTS = {
   UTIL_AVATARS: 'util/avatars',
-  UTIL_COLORS: 'util/colors'
+  UTIL_COLORS: 'util/colors',
+  AUTH_REGISTER: 'auth/register',
+  AUTH_LOGIN: 'auth/login'
 };
 
 interface IApiResponse {
@@ -31,5 +33,9 @@ export class RestService  implements IRestService {
 
   getColors() {
     return firstValueFrom(this.http.get<IApiResponse>(this.baseUrl+API_ENDPOINTS.UTIL_COLORS))
+  }
+
+  register(form: any) {
+    return this.http.post<IApiResponse>(this.baseUrl+API_ENDPOINTS.AUTH_REGISTER, form)
   }
 }
